87fdbb447451141114cad46647b63f70e7870780
AnkurVyas-BTC <ankur.vyas@botreeconsulting.com>
2018-02-17 19:25:15 +0530

    Pushing the gemset file

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'comparisons/compare'
   post 'comparisons/compare_sections'
   root to: 'comparisons#compare'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
